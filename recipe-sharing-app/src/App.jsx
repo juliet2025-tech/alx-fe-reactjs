cb1ac03c2e146f9823228ae6b21e4b34f81502ed
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-28 07:35:29 +0100

    recipe-sharing ap

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 import Snackbar from './components/Snackbar';
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-96 p-6 rounded-lg shadow-md bg-white text-center">
-        <h1 className="text-2xl font-semibold mb-6">
-          Recipe Sharing App
-        </h1>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="flex justify-center items-center h-screen">
+              <div className="w-96 p-6 rounded-lg shadow-md bg-white text-center">
+                <h1 className="text-2xl font-semibold mb-6">
+                  Recipe Sharing App
+                </h1>
 
-        <AddRecipeForm />
-        <RecipeList />
-        <Snackbar />
-      </div>
-    </div>
+                <AddRecipeForm />
+                <RecipeList />
+                <Snackbar />
+              </div>
+            </div>
+          }
+        />
+
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
