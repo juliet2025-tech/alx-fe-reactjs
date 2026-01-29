b49553f39c1f55627dd669643b1ee8ee03d08690
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-29 17:27:46 +0100

    git users

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
+      <h1 className="text-red-500 text-3xl font-bold">GitHub User Search</h1>
+
       <Search />
     </div>
   );
