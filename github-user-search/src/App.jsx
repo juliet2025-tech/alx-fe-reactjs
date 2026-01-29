224ce4a3f93382d3af596aa8746c170a3eb1d435
ojide chibundo <chibundojuliet21@gmail.com>
2026-01-29 16:52:59 +0100

    github users

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,17 @@
+
+
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Search for GitHub users</p>
+      <Search />
     </div>
   );
 }
 
 export default App;
+
 
