a9450a43cf82fed39fc5c721f53ab0a07b002ec6
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-06 19:47:21 +0100

    integration

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ function UserProfile() {
     <div
       className="
         bg-gray-100
-        p-4 md:p-8
-        max-w-xs md:max-w-sm
+        sm:p-4 md:p-8
+        sm:max-w-xs md:max-w-sm
         mx-auto my-20
         rounded-lg shadow-lg
       "
@@ -14,14 +14,15 @@ function UserProfile() {
         alt="User"
         className="
           rounded-full
-          w-24 h-24 md:w-36 md:h-36
+          sm:w-24 sm:h-24
+          md:w-36 md:h-36
           mx-auto
         "
       />
 
       <h1
         className="
-          text-lg md:text-xl
+          sm:text-lg md:text-xl
           text-blue-800
           my-4
           text-center
@@ -32,8 +33,8 @@ function UserProfile() {
 
       <p
         className="
+          sm:text-sm md:text-base
           text-gray-600
-          text-sm md:text-base
           text-center
         "
       >
