cf6b682d2ce842af0a32b25307f3a2b2682db641
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-06 19:29:08 +0100

    react-integration

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from 'react'
+import UserProfile from './components/UserProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-4xl font-bold mb-6">
-        Vite + React + Tailwind 🚀
-      </h1>
-
-      <button
-        onClick={() => setCount(count + 1)}
-        className="px-6 py-3 bg-indigo-600 hover:bg-indigo-700 rounded-lg text-lg font-semibold transition"
-      >
-        Count is {count}
-      </button>
-
-      <p className="mt-6 text-slate-400">
-        Edit <code className="text-indigo-400">src/App.jsx</code> and save
-      </p>
+    <div className="min-h-screen bg-slate-200 flex items-center justify-center">
+      <UserProfile />
     </div>
   )
 }
