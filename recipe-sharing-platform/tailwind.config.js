ae6566c1e05a436dac28b13be29d8b177e2f3dfe
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-15 19:11:33 +0100

    platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",          // root index.html (Vite)
+    "./src/**/*.{js,jsx,ts,tsx}" // all React source files
   ],
   theme: {
     extend: {},
