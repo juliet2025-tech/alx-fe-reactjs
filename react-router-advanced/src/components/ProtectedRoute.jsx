4d224f29b8c1ca7a41baf59bc316d5edab55ceaf
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-22 17:18:25 +0100

    react-router

---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+
+  const isAuthenticated =
+    localStorage.getItem("isAuthenticated");
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
